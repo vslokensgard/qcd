832d3e10a581c510c0fa372e130fac8b6de7912d
Sigrid Lokensgard <slokens1@swarthmore.edu>
2024-06-10 17:00:34 -0400

    dispersion relation

--- pionkaon.py
+++ pionkaon.py
@@ -1,7 +1,7 @@
-from astropy.io import ascii
-from astropy import table
-from astropy.table import Table
-from astropy.table import Column
+#from astropy.io import ascii
+#from astropy import table
+#from astropy.table import Table
+#from astropy.table import Column
 import numpy as np
 import matplotlib.pyplot as plt
 import os
@@ -14,10 +14,10 @@ import os
 def searchDir(keywords, path=None):
     if path:
         all_files = [(path + k) for k in os.listdir(path)]
-        
+
     else:
         all_files = os.listdir()
-        
+
     subset = [k for k in all_files if any(keyword in k for keyword in keywords)]
     return subset
 
@@ -28,19 +28,30 @@ def searchDir(keywords, path=None):
 # Returns:    NumPy table of input data
 #             Columns represent input files
 #             Rows represent time slices
-def mergeTables(file_list, names, selected):
-    merged = []
-    
-    # Add selected columns from each file to a single table 
-    for i in range(len(file_list)):
-        current_file = ascii.read(file_list[i])
-        current_table = Table(data=current_file, names=names)
-        trial_data = Column(data=current_table[selected], name=str(i)).data
-        merged.append(trial_data)
+#def mergeTables(file_list, names, selected):
+#    merged = []
+#
+#    # Add selected columns from each file to a single table
+#    for i in range(len(file_list)):
+#        current_file = ascii.read(file_list[i])
+#        current_table = Table(data=current_file, names=names)
+#        trial_data = Column(data=current_table[selected], name=str(i)).data
+#        merged.append(trial_data)
+#
+#    merged_array = np.asarray(merged)
+#    final_array = np.transpose(merged_array)
+#    return final_array
 
-    merged_array = np.asarray(merged)
-    final_array = np.transpose(merged_array)
-    return final_array
+def mergeTables(file_list, col_index):
+        by_file = np.zeros((len(file_list),64))
+
+        for i in range(len(file_list)):
+                current_file = np.genfromtxt(file_list[i])
+                by_param = np.transpose(current_file)
+                by_file[i] = by_param[col_index]
+
+        by_slice = np.transpose(by_file)
+        return by_slice
 
 # Purpose:    calculate table of jacknife bins for input data
 # Parameters: NumPy table DATA:
@@ -52,13 +63,13 @@ def mergeTables(file_list, names, selected):
 def jacknifeAverage(data):
     binned = np.zeros(data.shape)
     num_slices, num_bins = data.shape
-    
+
     for i in range(num_slices):
         row_sum = np.sum(data[i])
-        
+
         for j in range(num_bins):
             binned[i][j] = row_sum - data[i][j]
-      
+
     binned /= (num_bins - 1)
     return binned
 
@@ -74,19 +85,19 @@ def effectiveEnergy(bins):
     num_bins = bins.shape[1]
     in_by_bin = np.transpose(bins)
     out_by_bin = np.zeros((num_bins, num_intervals))
-    
+
     if (np.any(in_by_bin <= 0)):
         print("Warning: binned data includes negative or zero values.")
         print("Effective energies for these intervals will be set to 0.")
-    
+
     for bin_no in range(num_bins):
-        
+
         for int_no in range(num_intervals):
             if (in_by_bin[bin_no][int_no] <= 0) or (in_by_bin[bin_no][int_no+1] <= 0):
                 out_by_bin[bin_no][int_no] = 0
             else:
                 out_by_bin[bin_no][int_no] = np.log(in_by_bin[bin_no][int_no] / in_by_bin[bin_no][int_no+1])
-    
+
     final_bins = np.transpose(out_by_bin)
     return final_bins
 
@@ -100,7 +111,7 @@ def averageColumns(vals):
     num_intervals, num_trials = vals.shape
     avg = np.sum(vals, axis=1)
     avg /= num_trials
-    
+
     return avg
 
 # Purpose:    calculate jacknife error across all bins for each timeslice
@@ -117,15 +128,15 @@ def jacknifeError2D(binned_data, rho=None):
         rho_bars = rho
     else:
         rho_bars = averageColumns(binned_data)
-    
+
     for slice_no in range(num_slices):
         rho_bar = rho_bars[slice_no]
         sigma = 0
-        
+
         for bin_no in range(num_bins):
             diff = binned_data[slice_no][bin_no] - rho_bar
             sigma += np.square(diff)
-            
+
         delta_rho[slice_no] = np.sqrt(sigma * m_factor)
 
     return delta_rho
@@ -137,14 +148,14 @@ def jacknifeError2D(binned_data, rho=None):
 def maxSlice(jacknife_err, max_fact):
     starting_length = len(jacknife_err)
     err_threshold = jacknife_err[1] * max_fact
-    
+
     for i in range(1, starting_length):
         if jacknife_err[i] > err_threshold:
             break
-    
+
     if i < 5:
         i = 5
-        
+
     return i
 
 # Purpose:    graph effective energy values with jacknife error for each bin
@@ -157,28 +168,28 @@ def maxSlice(jacknife_err, max_fact):
 #             list of integers SELECTED_BINS, if not None then only graph listed indeces
 # Returns:    void
 def jacknifeGraph(eff_energies, jack_error, err_cutoff=None, selected_bins=None):
-    energies_by_bin = np.transpose(eff_energies)  
+    energies_by_bin = np.transpose(eff_energies)
     num_bins, num_intervals = energies_by_bin.shape
     min_slice = 1
     if err_cutoff:
         max_slice = maxSlice(jack_error, err_cutoff)
     else:
         max_slice = num_intervals
-    
+
     x = range(min_slice, max_slice)
     jack_error = jack_error[min_slice:max_slice]
-    
+
     if selected_bins:
         to_plot = selected_bins
     else:
         to_plot = list(range(num_bins))
-    
+
     for bin_no in to_plot:
         y = energies_by_bin[bin_no][min_slice:max_slice]
         plt.scatter(x, y)
         plt.errorbar(x, y, yerr=jack_error, fmt="o")
         plt.show()
-        
+
     return
 
 # Purpose:    identify the start indexes of plateaus of a given length, excluding index 0
@@ -192,13 +203,13 @@ def minSlice(ycoords, yerr, length=3):
     count = 0
     max_err = 0
     min_err = 0
-    
+
     while i < num_coords:
         if count == length:
             return i - count
-        
+
         else:
-            
+
             if (max_err - min_err) > 0:
                 curr_max = ycoords[i] + yerr[i]
                 curr_min = ycoords[i] - yerr[i]
@@ -208,7 +219,7 @@ def minSlice(ycoords, yerr, length=3):
                     min_err = curr_min
                 i += 1
                 count += 1
-                
+
             else:
                 i -= count
                 i += 1
@@ -217,7 +228,7 @@ def minSlice(ycoords, yerr, length=3):
                 max_err = curr_max
                 min_err = curr_min
                 count = 0
-            
+
     return 0
 
 # Purpose:    wrapper function to run multiple bins of data through minSlice()
@@ -233,10 +244,10 @@ def listPlateaus(binned_vals, binned_err, length=3, selected_bins=None):
     else:
         to_plot = range(len(vals_by_bin))
     plateaus = np.zeros(len(to_plot))
-    
+
     for bin_no in to_plot:
         plateaus[bin_no] = minSlice(vals_by_bin[bin_no], binned_err, length=length)
-        
+
     return plateaus
 
 # Purpose:    calculate one jacknife error value
@@ -248,11 +259,11 @@ def jacknifeError1D(rhos, avg=None):
         rho_bar = avg
     else:
         rho_bar = np.mean(rhos)
-        
+
     m_fact = (len(rhos) - 1) / len(rhos)
     sig_fact = np.sum( np.square(rhos - rho_bar) )
     delta_t = np.sqrt(m_fact * sig_fact)
-    
+
     return delta_t
 
 # Purpose:    calculate plateau fit value for one data bin
@@ -265,12 +276,12 @@ def jacknifeError1D(rhos, avg=None):
 def getPlateauFit(ycoords, yerr, min_slice, max_slice):
     if np.any(ycoords[min_slice:max_slice] <= 0) or np.any(yerr[min_slice:max_slice] <= 0):
         return -1
-    
+
     num = np.sum(ycoords[min_slice:max_slice] * np.power(yerr[min_slice:max_slice], -2))
     den = np.sum(np.power(yerr[min_slice:max_slice], -2))
     if (num == 0) or (den == 0):
         return -1
-    
+
     quo = num/den
     return quo
 
@@ -286,12 +297,12 @@ def plotPlateauFit(ycoords, yerr, min_slice, max_slice, delta=None, show=False):
     plat_fit = getPlateauFit(ycoords, yerr, min_slice, max_slice)
     if plat_fit < 0:
         return None
-    
+
     if delta:
         plat_err = delta
     else:
         plat_err = jacknifeError1D(ycoords[min_slice:max_slice], avg=plat_fit)
-    
+
     if show:
         title_fit = str(round(plat_fit, 5))
         title_err = str(round(plat_err, 5))
@@ -306,7 +317,7 @@ def plotPlateauFit(ycoords, yerr, min_slice, max_slice, delta=None, show=False):
         ymin = plat_fit - (point_range * 2)
         ymax = plat_fit + (point_range * 2)
         ax.set_ylim(ymin, ymax)
-    
+
         plt.title(plot_title)
         plt.xlabel(plot_label)
         plt.scatter(x, y)
@@ -316,7 +327,7 @@ def plotPlateauFit(ycoords, yerr, min_slice, max_slice, delta=None, show=False):
         plt.axhline(y=plat_fit-plat_err, linestyle=":")
         plt.axhline(y=plat_fit+plat_err, linestyle=":")
         plt.show()
-    
+
     return [plat_fit, plat_err]
 
 # Purpose:    calculate & display plateau fit starting points whose averages fall within the jacknife margin of error
@@ -329,22 +340,22 @@ def plotPlateauFit(ycoords, yerr, min_slice, max_slice, delta=None, show=False):
 def altFits1D(coords, err, static_bound, varied_bound, frac_delta=None, show=False):
     if varied_bound <= static_bound:
         rho_bar = getPlateauFit(coords, err, varied_bound, static_bound)
-        
+
         if rho_bar <= 0:
             print("Plateau fit from slices",varied_bound,"to",static_bound,"was not calculated due to junk values")
             return -1
-        
+
         delta_e = jacknifeError1D(coords[varied_bound:static_bound], avg=rho_bar)
         if delta_e <= 0:
             print("Plateau fit from slices",varied_bound,"to",static_bound,"was not calculated due to junk values")
             return -1
-        
+
         plotPlateauFit(coords, err, varied_bound, static_bound, show=show)
         bound = delta_e
         if frac_delta:
             if (delta_e/rho_bar < frac_delta):
                 bound = rho_bar * frac_delta
-        
+
         while varied_bound > 0:
             next_slice = varied_bound - 1
             include_next_plateau = getPlateauFit(coords, err, next_slice, static_bound)
@@ -356,45 +367,45 @@ def altFits1D(coords, err, static_bound, varied_bound, frac_delta=None, show=Fal
                 varied_bound = next_slice
                 rho_bar = getPlateauFit(coords, err, static_bound, varied_bound)
                 plotPlateauFit(coords, err, varied_bound, static_bound, delta=bound, show=show)
-                
+
             else:
                 return [rho_bar, bound]
-                
+
     else:
         rho_bar = getPlateauFit(coords, err, static_bound, varied_bound)
-        
+
         if rho_bar <= 0:
             print("Plateau fit from slices",static_bound,"to",varied_bound,"was not calculated due to junk values")
             return -1
-        
+
         delta_e = jacknifeError1D(coords[varied_bound:static_bound], avg=rho_bar)
-        
+
         if delta_e <= 0:
             print("Plateau fit from slices",varied_bound,"to",static_bound,"was not calculated due to junk values")
             return -1
-        
-        plotPlateauFit(coords, err, static_bound, varied_bound, show=show)        
+
+        plotPlateauFit(coords, err, static_bound, varied_bound, show=show)
         if (delta_e/rho_bar < frac_delta):
             bound = rho_bar * frac_delta
         else:
             bound = delta_e
-        
+
         while varied_bound < len(coords):
             next_slice = varied_bound + 1
             include_next_plateau = getPlateauFit(coords, err, static_bound, next_slice)
-            
+
             if include_next_plateau <= 0:
                 print("Plateau fit from slices",static_bound,"to",varied_bound,"was not calculated due to junk values")
                 return -1
-            
+
             if (rho_bar-bound) <= include_next_plateau <= (rho_bar+bound):
                 varied_bound = next_slice
                 rho_bar = getPlateauFit(coords, err, static_bound, varied_bound)
                 plotPlateauFit(coords, err, static_bound, varied_bound, delta=bound, show=show)
-                
+
             else:
                 return [rho_bar, bound]
-            
+
 # Purpose:    wrapper function to call altFits1D() on multiple data bins
 # Parameters: 2D NumPy array BINNED where rows represent timeslices & columns represent bins
 #             1D NumPy array ERR representing jacknife error values for each timeslice
@@ -411,26 +422,26 @@ def altFits1D(coords, err, static_bound, varied_bound, frac_delta=None, show=Fal
 #             key "max" for maximum bound when maximum slice is adjusted up within margin of error
 def altFits2D(binned, err, show=False, mode=0, selected_bins=None, length=3, max_fact=10, delta_fact=None):
     sort_by_bin = np.transpose(binned)
-    
+
     if selected_bins:
         to_plot = selected_bins
     else:
         to_plot = range(len(sort_by_bin))
     fit_vals = []
-         
+
     for bin_no in to_plot:
         if show: print("Bin no.", bin_no)
         bin_data = sort_by_bin[bin_no]
         plateau_min = minSlice(bin_data, err, length=length)
         plateau_max = maxSlice(err, max_fact=max_fact)
-        
+
         if mode == 0:
             fit_vals.append(plotPlateauFit(bin_data, err, plateau_min, plateau_max)[0])
         if mode == 1:
             fit_vals.append(altFits1D(bin_data, err, plateau_max, plateau_min, delta_fact, show=show)[0])
         if mode == 2:
             fit_vals.append(altFits1D(bin_data, err, plateau_min, plateau_max, delta_fact, show=show)[0])
-      
+
     return fit_vals
 
 # Purpose: list ground state value & error from a raw data table
@@ -448,8 +459,8 @@ def listGroundStates(data_tables, selected_col, mode=0):
     momentum_points = np.zeros((2,len(data_tables)))
     for i in range(len(data_tables)):
         data = mergeTables(data_tables[i], col_names, selected_col)
-        momentum_points[0][i], momentum_points[1][i] = getGroundState(data, mode=mode)    
-    
+        momentum_points[0][i], momentum_points[1][i] = getGroundState(data, mode=mode)
+
     return momentum_points
 
 def plotDispersion(momenta, vals_by_param):
@@ -457,14 +468,14 @@ def plotDispersion(momenta, vals_by_param):
     x = momenta
     y = vals_by_param[0]
     e = vals_by_param[1]
-    
+
     fit = np.polyfit(x, y, 2)
     fit_func = np.poly1d(fit)
     polyline = np.linspace(0, len(momenta), 100)
     plt.scatter(x, y)
     plt.errorbar(x, y, yerr=e, fmt="o")
-    plt.plot(polyline, fit_func(polyline)) 
-    
-    plt.show() 
+    plt.plot(polyline, fit_func(polyline))
+
+    plt.show()
     print(str(fit_func))
     return
