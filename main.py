2d1eda99b8717c73a67581c5d2afe100cbf4ae38
vslokensgard <tuu12896@temple.edu>
2024-06-11 11:59:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+import pionkaon as pk
 pairs = [[0, ["2pt_0"]], [1, ["2pt_1"]]]
-gs = listGroundStates(pairs, "./2pt_data/", 4, mode=0)
+gs = pk.listGroundStates(pairs, "./2pt_data/", 4, mode=0)
 print(gs)
-plotDispersion([0,1], gs)
+pk.plotDispersion([0,1], gs)
