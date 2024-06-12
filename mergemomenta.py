1e57458089ea93e2022b5e75049a10a4a4340ee0
vslokensgard <tuu12896@temple.edu>
2024-06-12 10:34:47 -0400

    Create mergemomenta.py

---
+++ mergemomenta.py
@@ -0,0 +1,14 @@
+import averager as avg
+import os
+
+#avg.momenta("../2pt/", ["twop_momforsmear_+0_+0_-1", "twop_momforsmear_+0_+0_+1"], "one")
+#avg.momenta("../2pt/", ["twop_momforsmear_+0_+0_+0"], "zero")
+#avg.momenta("../2pt/", ["twop_momforsmear_+0_+0_-2", "twop_momforsmear_+0_+0_+2"], "two")
+#avg.momenta("../2pt/", ["twop_momforsmear_+0_+0_-3", "twop_momforsmear_+0_+0_+3"], "three")
+#avg.momenta("../2pt/", ["twop_momforsmear_+0_+0_-4", "twop_momforsmear_+0_+0_+4"], "four")
+#avg.momenta("../2pt/", ["twop_momforsmear_+0_+0_-5", "twop_momforsmear_+0_+0_+5"], "five")
+
+path = "averaged/"
+momentum_files = os.listdir(path)
+#print (momentum_files)
+avg.splitFiles(momentum_files, path=path)
