b2e6316ab9f733c0f45ae8ca610bdac61c45174d
vslokensgard <tuu12896@temple.edu>
2024-06-12 16:48:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import pionkaon as pk
 
-query = "energy"
-p = [ (0, [query, "zero"]), (1, [query, "one"]), (2, [query, "two"]), (3, [query, "three"]), (4, [query, "four"]), (5, [query, "five"]) ]
+p = [ (0, ["zero"]), (1, ["one"]), (2, ["two"]), (3, ["three"]), (4, ["four"]), (5, ["five"]) ]
 path = "./"
 col_index = 4
-ground_states = pk.listGroundStates(p, path, col_index, mode=0)
-np.savetxt("default_settings", ground_states)
+ground_states = pk.listGroundStates(p, "data-energy/", col_index, mode=0)
